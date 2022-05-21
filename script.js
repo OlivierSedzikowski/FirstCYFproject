273b72190921658969e11375f0bbd7a7b71f2544
olivier sedzikowski <105029138+OlivierSedzikowski@users.noreply.github.com>
2022-05-21 15:01:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+new_li.innerText = 'banana';
+
+element_list.appendChild(new_li);
+let new_li2 = document.createElement('li');
+new_li2.innerText = 'grappes';
+
+element_list.appendChild(new_li2);
+let new_li3 = document.createElement('li');
+new_li3.innerText = 'watermelon';
+
+element_list.appendChild(new_li3);
